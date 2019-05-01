ed065753d96b7e9065c366dbb6f7cf9856cbfa25
kiwi <aaaokaaabbb@ybb.ne.jp>
2019-05-01 14:42:21 +0900

    結果を見やすくして、しばんを書いておきました

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
+#! /usr/bin/python3.7
 from pprint import pprint
 
 
 def test_update(file_path: str = 'sample.txt') -> None:
     with open(file_path, mode='rt', encoding='utf-8') as f:
         lists: list = [j for i in f.readlines() for j in i.split()]
-    pprint(lists)
-    print('-' * 20)
+    print('*' * 20)
     pprint(lists[8::4])
+    print('*' * 20)
 
 
 test_update(file_path='sample.txt')
