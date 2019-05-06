99412311d988d61dab0c49921167ef4669e5e219
kiwi <aaaokaaabbb@ybb.ne.jp>
2019-05-06 18:16:55 +0900

    破線を=に変更しました

--- test.py
+++ test.py
@@ -5,9 +5,9 @@ from pprint import pprint
 def test_update(file_path: str = 'sample.txt') -> None:
     with open(file_path, mode='rt', encoding='utf-8') as f:
         lists: list = [j for i in f.readlines() for j in i.split()]
-    print('*' * 20)
+    print('=' * 20)
     pprint(lists[8::4])
-    print('*' * 20)
+    print('=' * 20)
 
 
 test_update(file_path='sample.txt')
